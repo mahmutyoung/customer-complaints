8c9444611b1b8f127cc21bd680f8019b8e931bfa
Mahmut Genc <54088505+mahmutyoung@users.noreply.github.com>
2023-02-03 22:15:36 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+//import "./styles.css";
+function Message() {
+  const [emailInput, setEmailInput]=useState('')
+  const submitHandler=()=>{}
+  return(
+  <form onSubmit={submitHandler}>
+    <h1>Get in Contact </h1>
+    <input type='email' value={emailInput} placeholder='Your Email Address' ></input>
+    <br></br>
+    <br></br>
+    <textarea value='' placeholder='Write Your Message...'/>
+  </form>);
+}
+
+export default function App() {
+  return (
+    <div className="App" style={{border: '1px solid black', margin: '16px', padding: '8px'}}>
+      <Message/>
+      
+    </div>
+  );
+}
